01174975c6e7a16bfd24b480a3a6b6df787a0c20
Alexey Bagno <alexey.bagno@gmail.com>
2025-08-13 19:37:39 +0500

    Init impl of icons

--- lua/buffers/formatters.lua
+++ lua/buffers/formatters.lua
@@ -1,5 +1,7 @@
 local M = {}
 
+local devicons_loaded, devicons = pcall(require, "nvim-web-devicons")
+
 ---@alias formatter fun(full_path: string): string, integer[]|nil returns formatted string and hl range
 
 ---range is end-exclusive path name without the tail
@@ -7,6 +9,16 @@ local M = {}
 function M.relative_path(full_path)
 	local rel_path = vim.fn.fnamemodify(full_path, ":~:.")
 	local dir_name = vim.fn.fnamemodify(rel_path, ":h")
+	if devicons_loaded and vim.g.buffers_config.icon then
+		local icon, _ = devicons.get_icon(
+			vim.fn.fnamemodify(full_path, ":t"),
+			vim.fn.fnamemodify(full_path, ":e"),
+			{ default = true }
+		)
+		if icon then
+			rel_path = icon .. " " .. rel_path
+		end
+	end
 	if dir_name == "." then
 		return rel_path
 	end
@@ -18,6 +30,16 @@ end
 function M.filename_first(full_path)
 	local file_name = vim.fn.fnamemodify(full_path, ":t")
 	local dir_name = vim.fn.fnamemodify(full_path, ":~:.:h")
+	if devicons_loaded and vim.g.buffers_config.icon then
+		local icon, _ = devicons.get_icon(
+			vim.fn.fnamemodify(full_path, ":t"),
+			vim.fn.fnamemodify(full_path, ":e"),
+			{ default = true }
+		)
+		if icon then
+			file_name = icon .. " " .. file_name
+		end
+	end
 	if dir_name == "." then
 		return file_name
 	end
