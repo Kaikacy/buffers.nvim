b8df2aedbd83d43097887d48944b47c9f0471ec1
Kaikacy <lukakuka735@gmail.com>
2026-05-29 17:13:35 +0400

    Update formatters

--- lua/buffers/formatters.lua
+++ lua/buffers/formatters.lua
@@ -1,37 +1,23 @@
+---@type table<string, buffers.formatter>
 local M = {}
 
 ---@alias buffers.highlight [string, string[]|string] text, hl-group
----@alias buffers.formatter fun(full_name: string, buf: integer): buffers.highlight[]
+---@alias buffers.formatter fun(full_path: string, buf: integer): buffers.highlight[]
 
----@type buffers.formatter
-function M.relative_path(full_name)
-	local rel_path = vim.fn.fnamemodify(full_name, ":~:.")
-	local dir_name = vim.fn.fnamemodify(rel_path, ":h")
-	if dir_name == "." then return { { rel_path, "NormalFloat" } } end
+function M.relative_path(full_path)
+	local file_name = vim.fn.fnamemodify(full_path, ":t")
+	local path_head = vim.fn.fnamemodify(full_path, ":~:.:h")
+	if path_head == "." then return { { file_name, "NormalFloat" } } end
 
-	local file_name = vim.fn.fnamemodify(rel_path, ":t")
-	return { { dir_name .. "/", "Comment" }, { file_name, "NormalFloat" } }
+	return { { path_head .. "/", "Comment" }, { file_name, "NormalFloat" } }
 end
 
--- TODO
----@type buffers.formatter
 function M.filename_first(full_path)
 	local file_name = vim.fn.fnamemodify(full_path, ":t")
-	local dir_name = vim.fn.fnamemodify(full_path, ":~:.:h")
+	local path_head = vim.fn.fnamemodify(full_path, ":~:.:h")
+	if path_head == "." then return { { file_name, "NormalFloat" } } end
 
-	if devicons_loaded and vim.g.buffers_config.icons then
-		local icon, _ = devicons.get_icon(
-			vim.fn.fnamemodify(full_path, ":t"),
-			vim.fn.fnamemodify(full_path, ":e"),
-			{ default = true }
-		)
-		if icon then file_name = icon .. " " .. file_name end
-	end
-
-	if dir_name == "." then return file_name end
-
-	local formatted = file_name .. " " .. dir_name
-	return formatted, { #formatted - #dir_name, #formatted }
+	return { { file_name .. " ", "NormalFloat" }, { path_head, "Comment" } }
 end
 
 return M
